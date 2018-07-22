8bcab26b9670cab31a65a389124ef3478911b396
Christian Pena <christianp@tarte.com>
2018-07-22 16:00:13 -0400

    added bootstrap and configured it with gulp

--- gulpfile.js
+++ gulpfile.js
@@ -5,7 +5,8 @@ var gulp = require('gulp'),
     open = require('gulp-open'), // Open a URL in a web browser
     browserify = require('browserify'), // Bundles js
     reactify = require('reactify'), // Transform react jsx to js
-    source = require('vinyl-source-stream'); // Use conventional text streams with gulp
+    source = require('vinyl-source-stream'), // Use conventional text streams with gulp
+    concat = require('gulp-concat'); // Concatenates files
 
 var config = {
     port: 3000,
@@ -13,6 +14,10 @@ var config = {
     paths: {
         html: './src/*.html',
         js: './src/**/*.js',
+        css: [
+            'node_modules/bootstrap/dist/css/bootstrap.min.css',
+            'node_modules/bootstrap/dist/css/bootstrap-theme.min.css'
+        ],
         dist: './dist',
         mainJs: './src/main.js'
     }
@@ -41,6 +46,10 @@ gulp.task('html', function() {
     .pipe(connect.reload());
 });
 
+/*
+    JS Task to take js file and pass it through browserify, then convert js into react which we will then bundle,
+    check if there is any error, move it over to bundle.js file, move the file over to scrips and then refresh the page
+*/
 gulp.task('js', function() {
     browserify(config.paths.mainJs)
     .transform(reactify)
@@ -51,10 +60,18 @@ gulp.task('js', function() {
     .pipe(connect.reload());
 });
 
+
+/* CSS Task to take bootstrap files, concat it with bundles.css and then move the file to css folder */
+gulp.task('css', function() {
+    gulp.src(config.paths.css)
+    .pipe(concat('bundle.css'))
+    .pipe(gulp.dest(config.paths.dist + '/css'));
+});
+
 // watch for any changes in html files
 gulp.task('watch', function() {
     gulp.watch(config.paths.html, ['html']);
     gulp.watch(config.paths.js, ['js']);
 })
 
-gulp.task('default', ['html', 'js', 'open', 'watch']);
+gulp.task('default', ['html', 'js', 'css', 'open', 'watch']);
--- src/main.js
+++ src/main.js
@@ -1,3 +1,5 @@
+$ = jQuery = require('jquery'); // this allows us to use either $ sign or jQuery keyword to use jquery
+
 var App = console.log('Hello World');
 
 module.exports = App;
